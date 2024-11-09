a8eceab0a96ed6cee7974a01776b1cd056779691
Skydiver-WS <isu172@mail.ru>
2024-11-09 13:56:46 +0300

    Create project for reserved hotel Rest Service

---
+++ src/main/java/spw/reserved/hotel/rest/service/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package spw.reserved.hotel.rest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spw/reserved/hotel/rest/service/RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spw.reserved.hotel.rest.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
