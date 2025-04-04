a98815ceb6669d21474fc0c4fb4cf2a8ebda7ea8
Skydiver_WS <isu172@mail.ru>
2025-04-04 18:44:57 +0300

    Change id for photo

--- src/main/java/ru/project/reserved/system/hotel/rest/service/dto/Photo.java
+++ src/main/java/ru/project/reserved/system/hotel/rest/service/dto/Photo.java
@@ -3,10 +3,12 @@ package ru.project.reserved.system.hotel.rest.service.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 public class Photo {
-    private Long id;
+    private UUID id;
 
     private String photo;
 }
